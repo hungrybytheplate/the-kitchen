0d8a5099d5708c0a91f396b4366207eeb4e49535
Matt Reed <m4ttr33d@users.noreply.github.com>
2026-07-20 13:14:22 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'app.lovable.the_kitchen',
-  appName: 'the-kitchen',
+  appName: 'the-kitchen-hq',
   webDir: 'dist',
   server: {
     url: 'https://5d728605-e6a1-4efc-b457-1e1c28524295.lovableproject.com?forceHideBadge=true',
